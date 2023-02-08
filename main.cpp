bf35ff7dc5e6fd3fe58a00692e0144f798d1f96a
István Cserny <hobbi@cspista.hu>
2023-02-08 08:47:16 +0000

    test version

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+
+const double pi = 3.141592653589793238462;
+const double amplitude = 32767;               // 2^15-1
+
+// The sinewave is created on this pin
+// Adjust analog output pin name to your board spec.
+AnalogOut aout(PA_4);
+
+int main()
+{
+    double rads = 0.0;
+    uint16_t sample[36] = {0};
+
+    while (1) {
+        for (int i = 0; i < 36; i++) {       
+            aout.write_u16(sample[i]);
+            wait_us(63);
+        }
+    }
+}
